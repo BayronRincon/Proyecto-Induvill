b53264fac7d6c742347bee4371b2bf6ed10adcb0
Bayron Rincon <143965242+BayronRincon@users.noreply.github.com>
2024-10-01 10:16:11 -0500

    Add files via upload

---
+++ INDUVILL/Pagina_Catalogo/formulario1.php
@@ -0,0 +1,21 @@
+<?php
+
+include_once("")
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="post">
+        Nombre: <input type="text" name="nombre" required><br>
+        descripcion: <input type="text" name="descripcion" required><br>
+        Existencial_inicial: <input type="text" name="existencial_inicial" required><br>
+        <input type="submit" value="Insertar">
+    </form>
+</body>
+</html>
---
+++ INDUVILL/Pagina_Catalogo/login-register/index.php
@@ -0,0 +1,78 @@
+<?php
+
+ session_start();
+
+ if(isset($_SESSION['usuario'])){
+     header("location: pag_inventario.php");
+ }
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login y registro - INDUVILL</title>
+    <link rel="stylesheet" href="assets/css/estilos.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+<!--<button type="button" ><a href="http://127.0.0.1:5500/Pagina_Catalogo/index.html" class="nav-link">Inicio</button>-->
+
+
+    <main>
+
+        <div class="contenedor__todo">
+
+            <div class="caja__trasera">
+                <div class="caja__trasera-login">
+                    <h3>¿Ya tienes una cuenta?</h3>
+                    <p>Inicia sesion para entrar a la pagina</p>
+                    <button id="btn__iniciar-sesion">Iniciar sesion</button>
+
+                </div>
+            
+                <div class="caja__trasera-register">
+                    <h3>¿Aun no tienes una cuenta?</h3>
+                    <p>Registrate para que puedas iniciar sesion</p>
+                    <button id="btn__register">Registrarse</button>
+                    
+                </div>
+                
+            </div>
+
+            <div class="contenedor__login-register">
+
+                <form action="php/login_usuario_be.php" method="POST" class="formulario__login">
+                    <h2>Iniciar Sesion</h2>
+                    <input type="text" placeholder="Correo electronico" name="correo">
+                    <input type="password" placeholder="Contraseña" name="contrasena">
+                    <button>Entrar</button>
+                </form>
+
+                <form action="php/registro_usuario_be.php" method="POST" class="formulario__register">
+                    <h2>Registrarse</h2>
+                    <input type="text" placeholder="Nombre Completo" name="nombre_completo">
+                    <input type="text" placeholder="Correo electronico" name="correo">
+                    <input type="text" placeholder="Usuario" name="usuario">
+                    <input type="password" placeholder="Contraseña" name="contrasena">
+                    <button>Registrarse</button>
+                    
+                </form>
+                
+            </div>
+
+        </div>
+
+
+
+    </main>
+
+    <script src="assets/js/script.js"></script>
+    
+</body>
+</html>
